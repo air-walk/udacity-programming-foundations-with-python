40cd1a2f47d0c5725db9262cffd837fd400019e0
Air Walk <air-walk@users.noreply.github.com>
2017-06-04 13:00:28 -0400

    adding a loop

--- break_time.py
+++ break_time.py
@@ -1,5 +1,8 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=PMivT7MJ41M")
+i = 0
+while(i < 3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=PMivT7MJ41M")
+    i = i + 1
