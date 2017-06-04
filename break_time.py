d286449f7e38694a06d174422e1ba8b5a4e1a26d
Air Walk <air-walk@users.noreply.github.com>
2017-06-04 13:02:38 -0400

    recording start time with a print statement

--- break_time.py
+++ break_time.py
@@ -2,6 +2,7 @@ import time
 import webbrowser
 
 i = 0
+print("This program started on: " + time.ctime())
 while(i < 3):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=PMivT7MJ41M")
