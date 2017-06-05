ca8871337a45e64d2c5d97901795b809f922a0a9
Air Walk <air-walk@users.noreply.github.com>
2017-06-04 23:11:10 -0400

    refactoring

--- lesson-3/mindstorms.py
+++ lesson-3/mindstorms.py
@@ -1,28 +1,29 @@
 import turtle
 
 def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("red")
-    
     brad = turtle.Turtle()
     brad.shape("circle")
     brad.color("black")
     brad.speed(3)
 
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)    
+    i     = 0
+    times = 4
 
+    while(i < times):
+        brad.forward(100)
+        brad.right(90)
+        i = i + 1
+
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
-    angie.circle(100)    
-    
-    window.exitonclick()
+    angie.circle(100)
+
+window = turtle.Screen()
+window.bgcolor("red")
 
 draw_square()
+draw_circle()
+
+window.exitonclick()
