ee6e87d2a732e904747bdc5b07f1cc18cd6cb5da
Air Walk <air-walk@users.noreply.github.com>
2017-06-04 23:29:50 -0400

    more refactoring

--- lesson-3/mindstorms.py
+++ lesson-3/mindstorms.py
@@ -1,44 +1,37 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(0, 4):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_triangle(some_turtle):
+    for i in range(0, 3):
+        some_turtle.forward(100)
+        some_turtle.left(120)    
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    
     brad = turtle.Turtle()
     brad.shape("circle")
     brad.color("black")
     brad.speed(3)
+    draw_square(brad)
 
-    i     = 0
-    times = 4
-
-    while(i < times):
-        brad.forward(100)
-        brad.right(90)
-        i = i + 1
-
-def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
 
-def draw_triangle():
     bulla = turtle.Turtle()
     bulla.shape("turtle")
     bulla.color("green")
     bulla.speed(10)
+    draw_triangle(bulla)
 
-    i     = 0
-    times = 3
+    window.exitonclick()
 
-    while(i < times):
-        bulla.forward(100)
-        bulla.left(120)
-        i = i + 1
-    
-window = turtle.Screen()
-window.bgcolor("red")
+draw_art()
 
-draw_square()
-draw_circle()
-draw_triangle()
-
-window.exitonclick()
