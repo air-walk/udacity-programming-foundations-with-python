753e7988c6be8e164d50b69c01667114ec94ef60
Air Walk <air-walk@users.noreply.github.com>
2017-06-04 23:58:53 -0400

    using turtle to draw initials

--- lesson-3/mindstorms.py
+++ lesson-3/mindstorms.py
@@ -25,5 +25,52 @@ def draw_art():
 
     window.exitonclick()
 
-draw_art()
+def draw_A(some_turtle):
+    some_turtle.left(60)
+    some_turtle.forward(100)
+    some_turtle.right(120)
+    some_turtle.forward(65)
+    some_turtle.right(120)
+    some_turtle.forward(65)
+    some_turtle.penup()
+    some_turtle.right(180)
+    some_turtle.forward(65)
+    some_turtle.pendown()
+    some_turtle.right(60)
+    some_turtle.forward(100 - 65)
+    some_turtle.left(60)
 
+def draw_W(some_turtle):
+    some_turtle.penup()
+    some_turtle.left(90)
+    some_turtle.forward(80)
+    some_turtle.right(150)
+    some_turtle.pendown()
+    some_turtle.forward(85)
+    some_turtle.left(100)
+    some_turtle.forward(50)
+    some_turtle.right(90)
+    some_turtle.forward(50)
+    some_turtle.left(100)
+    some_turtle.forward(100)
+   
+def draw_initials():
+    window = turtle.Screen()
+    window.bgcolor("blue")
+
+    dude = turtle.Turtle()
+    dude.color("pink")
+    dude.speed(1)
+
+    draw_A(dude)
+
+    dude.penup()
+    dude.forward(20)
+    dude.pendown()
+    
+    draw_W(dude)
+
+    window.exitonclick()
+    
+# draw_art()
+draw_initials()
