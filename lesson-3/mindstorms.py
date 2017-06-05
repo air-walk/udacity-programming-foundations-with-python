d8f90623d96c9fba16649afd9e18c79c4060fb9b
Air Walk <air-walk@users.noreply.github.com>
2017-06-04 23:01:00 -0400

    using turtle to create a circle

--- lesson-3/mindstorms.py
+++ lesson-3/mindstorms.py
@@ -8,7 +8,7 @@ def draw_square():
     brad.shape("circle")
     brad.color("black")
     brad.speed(3)
-    
+
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
@@ -17,6 +17,11 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)    
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)    
     
     window.exitonclick()
 
