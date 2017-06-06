fcf0fdb326ad4409283890969c81c4651e53d2f9
Air Walk <air-walk@users.noreply.github.com>
2017-06-05 22:03:13 -0400

    adding profanity checker

--- lesson-5/check_profanity.py
+++ lesson-5/check_profanity.py
@@ -1,7 +1,16 @@
+import urllib
+
 def read_text():
     quotes = open("/home/air-walk/workspaces/udacity-programming-foundations-with-python/lesson-5/movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
+    check_profanity(contents_of_file)
     quotes.close()
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output     = connection.read()
+    print(output)
+    connection.close()
 
 read_text()
