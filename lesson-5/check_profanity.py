ca76d2430dfcf1c9439f4d7723008df0a948bc7b
Air Walk <air-walk@users.noreply.github.com>
2017-06-05 22:07:26 -0400

    adding some print statements to emit human readable output

--- lesson-5/check_profanity.py
+++ lesson-5/check_profanity.py
@@ -13,4 +13,11 @@ def check_profanity(text_to_check):
     print(output)
     connection.close()
 
+    if "true" in output:
+        print("Profanity Alert!")
+    elif "false" in output:
+        print("This document has no curse words!")
+    else:
+        print("Couldn't scan the document properly :(")
+
 read_text()
