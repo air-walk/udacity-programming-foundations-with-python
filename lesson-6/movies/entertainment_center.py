8222cce3f87d5b3a42399fe843ef1dcf9826a0cf
Air Walk <air-walk@users.noreply.github.com>
2017-06-08 15:59:08 -0400

    adding movies

--- lesson-6/movies/entertainment_center.py
+++ lesson-6/movies/entertainment_center.py
@@ -13,9 +13,22 @@ avatar = media.Movie("Avatar",
 # print(avatar.storyline)
 # avatar.show_trailer()
 
-office_space = media.Movie("Office Space",
-                           "An inspirational saga from the corporate world",
-                           "https://upload.wikimedia.org/wikipedia/en/8/8e/Office_space_poster.jpg",
-                           "https://www.youtube.com/watch?v=dMIrlP61Z9s")
-print(office_space.storyline)
-office_space.show_trailer()
+school_of_rock = media.Movie("School of Rock",
+                             "Using rock music to learn",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+ratatouille = media.Movie("Ratatouille",
+                          "A rat is a chef in Paris",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Going back in time to meet authors",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+hunger_games = movie.Movie("Hunger Games",
+                           "A really real reality show",
+                           "https://upload.wikimedia.org/wikipedia/en/4/4b/Hunger_Games_Film_Poster.jpg",
+                           "https://www.youtube.com/watch?v=mfmrPu43DF8")
