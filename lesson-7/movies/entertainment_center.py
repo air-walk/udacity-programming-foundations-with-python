8b9f0ac108da8e1757c98648a60094978ab813a7
Air Walk <air-walk@users.noreply.github.com>
2017-07-06 11:02:12 -0400

    removing commented out code

--- lesson-7/movies/entertainment_center.py
+++ lesson-7/movies/entertainment_center.py
@@ -5,14 +5,11 @@ toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-# print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-# print(avatar.storyline)
-# avatar.show_trailer()
 
 school_of_rock = media.Movie("School of Rock",
                              "Using rock music to learn",
@@ -35,8 +32,6 @@ hunger_games = media.Movie("Hunger Games",
                            "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
-# fresh_tomatoes.open_movies_page(movies)
-# print(media.Movie.VALID_RATINGS)
 print(media.Movie.__doc__)
 print(media.Movie.__name__)
 print(media.Movie.__module__)
